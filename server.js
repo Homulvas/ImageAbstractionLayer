8719644b33e227b1f526ef86a49eb7362b6adc10
Ignas Kaziukėnas <ignas@ignas.co>
2017-04-13 23:21:20 +0300

    query google custom search api for images

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
-var mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/test2';
-var mongodb = require("mongodb");
+var mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/test2'
+var mongodb = require("mongodb")
 var mongo = mongodb.MongoClient
+https = require('https')
+
+var apiKey = process.env.GOOGLE_API || 'api'
+var cx = process.env.CX_KEY || 'cx' 
+
+var googleUrl = 'https://www.googleapis.com/customsearch/v1?key=' + apiKey + '&cx' + cx + '=&q=cat&searchType=image&alt=json&num=10&start=1'
 
 app.get('/search/*', function (req, res) {
     var query = req.params[0]
@@ -20,7 +26,26 @@ app.get('/search/*', function (req, res) {
 
         db.close();
     })
-    res.send('need to get search results for ' + query)
+
+    var queryUrl = 'https://www.googleapis.com/customsearch/v1?key=' + apiKey + '&cx=' + cx + '&q=' + query + '&searchType=image&alt=json&num=10&start=1';
+
+    https.get(queryUrl, function (response) {
+        var data = "";
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            data+=chunk
+        })
+        var result = []
+        response.on('end', function () {
+            var fullResponse = JSON.parse(data)
+            for (var i = 0; i < fullResponse.items.length; i ++) {
+                var item = fullResponse.items[i]
+                var image = {url: item.link, thumbnail: item.image.thumbnailLink, snippet: item.snippet}
+                result.push(image)
+            }
+            res.send(JSON.stringify(result))
+        })
+    })
 })
 
 app.get('/recent', function (req, res) {
@@ -30,14 +55,14 @@ app.get('/recent', function (req, res) {
         }
         var coll = db.collection('visits')
         var recent = coll.find({}, { _id: 0, query: 1, created_on: 1 })
-        .sort({ _id: -1 })
-        .limit(5)
-        .toArray(function (err, data) {
-            if (err) {
-                return res.send(err);
-            }
-            res.send(JSON.stringify(data))
-        })
+            .sort({ _id: -1 })
+            .limit(5)
+            .toArray(function (err, data) {
+                if (err) {
+                    return res.send(err);
+                }
+                res.send(JSON.stringify(data))
+            })
 
         db.close();
     })
